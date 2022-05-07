5b58f218711c1c2af5501d67b30902df7411dc3a
Taro Segawa(瀬川太朗） <tdog7305@icloud.com>
2022-05-07 13:07:41 +0900

    Initial Commit

---
+++ Gacha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gacha
+//
+//  Created by 瀬川太朗 on 2022/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
